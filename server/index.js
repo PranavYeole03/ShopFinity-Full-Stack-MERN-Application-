2d014fcea59568fe81b2c05fa4f9daccb0e96068
Pranav <yeolepranav13@example.com>
2025-10-21 01:17:47 +0530

    Updated Axios.js

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(
   })
 );
 
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (request, response) => {
   response.json({
@@ -47,7 +47,7 @@ app.use("/api/subCategory", subCategoryRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/address", addressRouter);
-app.use("/api/order",orderRouter)
+app.use("/api/order", orderRouter);
 
 connectDB().then(() => {
   app.listen(PORT, () => {
